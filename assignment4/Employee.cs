de6e70b89351fe26a58a22f96d21865ac9485216
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-15 17:51:31 +0530

    Add project files.

---
+++ assignment4/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace assignment4
+{
+    public class Employee
+    {
+       private  int eid;
+        private static int count = 0;
+       private string name;
+       private double hra, da, pt, pf,bs,total,netpaid;
+
+        public Employee(string name,double pt,double bs)
+        {
+            count++;
+            eid = count;
+            this.name = name;
+            this.pt = pt;
+            this.bs = bs;
+        }
+
+        public void Calculate()
+        {
+            hra = bs * 0.40;
+            da = bs * 0.20;
+            pf = bs * 0.12;
+
+            total = bs + hra + da + pf + pt;
+            netpaid = total - pf - pt;
+        }
+
+        public static  int DisplayCount()
+        {
+            
+            return count;
+        }
+        /* public string Display()
+         {
+             return $"employee detail: empid {eid} \nname {name} \nhra {hra} \nda {da} \npf {pf} \ntotalsalary {total} \nnetPaid {netpaid}";
+         }*/
+        public override string ToString()
+        {
+            return $"emp: \nempid {eid} \nname {name} \ntotalsalary {total} \nnetpaid {netpaid}";
+        }
+    }
+}
---
+++ assignment4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee("rekha", 200,45000);
+            Employee e1 = new Employee("surekha", 200, 50000);
+            e.Calculate();
+            e1.Calculate();
+
+            //Console.WriteLine(e.Display());
+            //Console.WriteLine(e1.Display());
+            Console.WriteLine(Employee.DisplayCount());
+
+            Console.WriteLine(e);
+            Console.WriteLine(e1);
+        }
+    }
+}
